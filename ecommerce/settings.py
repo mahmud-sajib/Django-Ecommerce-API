d7d17bec9f3f8d0581259a9c31e95aa9bad962e8
unknown <shout.mahmud@gmail.com>
2020-11-14 11:39:35 +0600

    msg

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -128,7 +128,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
 
 # Location that holds user-uploaded files.
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
